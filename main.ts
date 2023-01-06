e6d19f9c12b27fff9af33c95cbe6fb800f677efc
EliProm <121956774+EliProm@users.noreply.github.com>
2023-01-06 17:03:53 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,20 @@
 basic.showIcon(IconNames.Heart)
+basic.pause(500)
+basic.clearScreen()
+basic.showString(" EINLADUNG!")
 basic.pause(200)
-basic.showString(" Einladung!")
-basic.pause(200)
-basic.showString(" Druecke Logo")
 basic.showArrow(ArrowNames.North)
 while (true) {
     if (input.logoIsPressed()) {
         music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.OnceInBackground)
-        basic.showString("Druecke A")
         basic.showArrow(ArrowNames.West)
     }
     if (input.buttonIsPressed(Button.A)) {
         basic.showString(" Wann? - 22.01.2023 | 11:00 Uhr")
         basic.pause(500)
-        basic.showString("Druecke B")
         basic.showArrow(ArrowNames.East)
     }
     if (input.buttonIsPressed(Button.B)) {
         basic.showString(" Wo? - Wankstr. 4")
-        basic.pause(500)
     }
 }
